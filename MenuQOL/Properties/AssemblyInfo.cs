8a5726c33fb0403761a75023531df3583226b919
Stuart Gilbert <stuart.gilbert@gmail.com>
2026-03-16 00:09:38 -0700

    Trying to set up a release process

--- MenuQOL/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using MelonLoader;
-using MenuQOL;
-
-[assembly: MelonInfo(typeof(MenuQOLMod), "MenuQOL", "1.0.0", "Stuart Gilbert")]
-[assembly: MelonGame("The Sledding Corporation", "Sledding Game Demo")]
--- SnowmanMod/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using MelonLoader;
-using SnowmanMod;
-
-[assembly: MelonInfo(typeof(SnowmanModMain), "SnowmanMod", "1.0.0", "Stuart Gilbert")]
-[assembly: MelonGame("The Sledding Corporation", "Sledding Game Demo")]
